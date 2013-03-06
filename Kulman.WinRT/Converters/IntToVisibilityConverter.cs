1351a4830f87282ec3d34013bf688821803912ab
Igor Kulman <igor@kulman.sk>
2013-03-06 15:36:32 +0100

    + added README.md + added IntToVisibilityConverter

---
+++ Kulman.WinRT/Converters/IntToVisibilityConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Kulman.WinRT.Converters
+{
+    public class IntToVisibilityConverter: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            int count = (int)value;
+            return count <= 0 ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Kulman.WinRT
+
+Collection of useful utilities and components for WinRT applications.
+
+Get it from Nuget
+
+    PM> Install-Package Kulman.WinRT
