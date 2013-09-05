4ac5b638e0ae5b2963b8d023c33380549c0fcabb
Igor Kulman <igor@kulman.sk>
2013-09-05 09:51:42 +0200

    + some comments

--- Kulman.WinRT/Extensions/FolderExtensions.cs
+++ Kulman.WinRT/Extensions/FolderExtensions.cs
@@ -5,9 +5,13 @@ using Windows.Storage;
 namespace Kulman.WinRT.Extensions
 {
     public static class FolderExtensions
-    {
-       
-
+    {       
+        /// <summary>
+        /// Checks if a folder contains a file
+        /// </summary>
+        /// <param name="folder">Folder</param>
+        /// <param name="localFilename">Filename</param>
+        /// <returns>True if the folder contains the file</returns>
         public static async Task<bool> ContainsFileAsync(this StorageFolder folder,string localFilename)
         {
             //bool exists = await ApplicationData.Current.LocalFolder.ContainsFileAsync(localFilename);
